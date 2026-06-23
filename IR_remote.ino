50424c373d9745ff80bec959e3390e870e94375c
InLactose-Tolerant <dhairyadhairya.dhairyadhairya@gmail.com>
2026-06-23 23:38:33 +1000

    Code

--- IR_remote.ino
+++ IR_remote.ino
@@ -3,16 +3,27 @@
 // Define the IR receiver pin
 const int IR_RECEIVE_PIN = 12;
 uint32_t response;
+const int LED_1 = 9;
+const int LED_2 = 11;
+const int LED_3 = LED_3;
+const int LED_4 = 3;
+const int LED_5 = 6;
+
+const int IR_value1 = 3208706820
+const int IR_value2 = 4177984260
+const int IR_value3 = 4161272580
+const int IR_value4 = 3191995140
+const int IR_value5 = 3141860100
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
   pinMode(13, OUTPUT);
-  pinMode(6, OUTPUT);
+  pinMode(LED_5, OUTPUT);
   
   Serial.println("Starting IR-receiver");
 
@@ -35,30 +46,30 @@ void loop() {
     // Resume receiving for the next signal
     IrReceiver.resume();
 
-      if (response == 3208706820){
-        digitalWrite(9, HIGH);
+      if (response == IR_value1){
+        digitalWrite(LED_1, HIGH);
         delay(500);
-        digitalWrite(9, LOW);
+        digitalWrite(LED_1, LOW);
       }
-      if (response == 4177984260){
-        digitalWrite(11, HIGH);
+      if (response == IR_value2){
+        digitalWrite(LED_2, HIGH);
         delay(500);
-        digitalWrite(11, LOW);
+        digitalWrite(LED_2, LOW);
       }
-      if (response == 4161272580){
-        digitalWrite(6, HIGH);
+      if (response == IR_value3){
+        digitalWrite(LED_5, HIGH);
         delay(500);
-        digitalWrite(6, LOW);
+        digitalWrite(LED_5, LOW);
       }
-      if (response == 3191995140){
-        digitalWrite(3, HIGH);
+      if (response == IR_value4){
+        digitalWrite(LED_4, HIGH);
         delay(500);
-        digitalWrite(3, LOW);
+        digitalWrite(LED_4, LOW);
       }
-      if (response == 3141860100){
-        digitalWrite(2, HIGH);
+      if (response == IR_value5){
+        digitalWrite(LED_3, HIGH);
         delay(500);
-        digitalWrite(2, LOW);
+        digitalWrite(LED_3, LOW);
       }
    }
   }
